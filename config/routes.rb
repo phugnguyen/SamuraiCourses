9ceaae25ec79d66387978c96aa9a98e0ce5063e3
Phu <phugnguyen@gmail.com>
2019-04-30 14:21:14 -0700

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :users
+    resource :session, only: [:new, :create, :destroy]
+  end
+
 end
