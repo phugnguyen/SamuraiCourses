3dd666987c95874bc3c058ed76393ccd8fe43e13
Phu <phugnguyen@gmail.com>
2019-05-03 11:57:14 -0700

    Add test term dropdown menu

--- frontend/components/school/school_index.jsx
+++ frontend/components/school/school_index.jsx
@@ -9,9 +9,9 @@ class SchoolIndex extends React.Component {
     };
   }
 
-  handleSchoolClick(id) {
+  handleInput(type) {
     return e => {
-      this.setState({ school: id });
+      this.setState({ [type]: e.target.value });
     };
   }
 
@@ -23,19 +23,27 @@ class SchoolIndex extends React.Component {
     const { schools } = this.props;
     console.log(this.state);
     return (
-      <div className="school-term-select">
+      <form>
         <h2>Please select your school</h2>
 
         <ul className="school-select-index">
           {schools.map(school => (
-            <li onClick={this.handleSchoolClick(school.id)} key={school.id}>
+            <li onClick={this.handleInput("school")} value={school.id}>
               {school.name}
             </li>
           ))}
         </ul>
 
         <h2>Please select the term</h2>
-      </div>
+        <select value={this.state.term} onChange={this.handleInput("term")}>
+          <option value="summer-2019">Summer 2019</option>
+          <option value="spring-2019">Spring 2019</option>
+          <option value="winter-2019">Winter 2019</option>
+          <option value="fall-2018">Fall 2018</option>
+        </select>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
