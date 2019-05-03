4d4a5b876d13c532613e59106cb5092ce0d7a3dd
Phu <phugnguyen@gmail.com>
2019-05-02 17:30:01 -0700

    Add modal to app.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,9 +5,11 @@ import NavBarContainer from "./nav_bar/nav_bar_container";
 import Home from "./home/home";
 import SignUpContainer from "./session/signup_container";
 import LogInContainer from "./session/login_container";
+import Modal from "./modal/modal";
 
 export default () => (
   <div className="app">
+    <Modal />
     <Route path="/" component={NavBarContainer} />
     <Route exact path="/" component={Home} />
     <AuthRoute path="/register" component={SignUpContainer} />
