a5661e4d49c095a1e15b55b99ee8d3e70a613c46
Phu <phugnguyen@gmail.com>
2019-05-04 14:38:56 -0700

    style home page button

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default () => (
   <div className="home">
     <h1>Welcome to SamuraiCourses</h1>
-    <Link className="plan-schedule-link" to="/planSchedule">
+    <Link className="nav-btn" to="/planSchedule">
       Plan a schedule!
     </Link>
   </div>
