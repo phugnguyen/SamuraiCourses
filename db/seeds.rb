42bc88330372109086500293369e4607ff2d49d0
Phu <phugnguyen@gmail.com>
2019-05-05 21:02:32 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,103 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Term.delete_all
+School.delete_all
+User.delete_all
+
+User.create!({
+    username: 'guest',
+    password: 'password',
+    email: 'guest@password.com'
+})
+
+a = School.create!({
+    name: "UC Santa Barbara"
+})
+b = School.create!({
+    name: "UC Los Angeles"
+})
+c = School.create!({
+    name: "Stanford"
+})
+d = School.create!({
+    name: "CSU San Francisco"
+})
+
+Term.create!({
+    season: "Winter",
+    year: "2019",
+    school_id:a.id
+})
+
+Term.create!({
+    season: "Spring",
+    year: "2019",
+    school_id:a.id
+})
+Term.create!({
+    season: "Summer",
+    year: "2019",
+    school_id:a.id
+})
+Term.create!({
+    season: "Fall",
+    year: "2019",
+    school_id:a.id
+})
+Term.create!({
+    season: "Winter",
+    year: "2019",
+    school_id: b.id
+})
+Term.create!({
+    season: "Spring",
+    year: "2019",
+    school_id: b.id
+})
+Term.create!({
+    season: "Summer",
+    year: "2019",
+    school_id: b.id
+})
+Term.create!({
+    season: "Fall",
+    year: "2019",
+    school_id: b.id
+})
+Term.create!({
+    season: "Winter",
+    year: "2019",
+    school_id: c.id
+})
+Term.create!({
+    season: "Spring",
+    year: "2019",
+    school_id: c.id
+})
+Term.create!({
+    season: "Summer",
+    year: "2019",
+    school_id: c.id
+})
+Term.create!({
+    season: "Fall",
+    year: "2019",
+    school_id: c.id
+})
+Term.create!({
+    season: "Summer",
+    year: "2019",
+    school_id: d.id
+})
+Term.create!({
+    season: "Fall",
+    year: "2019",
+    school_id: d.id
+})
+Term.create!({
+    season: "Spring",
+    year: "2019",
+    school_id: d.id
+})
