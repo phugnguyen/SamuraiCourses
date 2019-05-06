e49c8621802bbdaecccaa132b94a1bd80ba3a945
Phu <phugnguyen@gmail.com>
2019-05-06 10:20:44 -0700

    adjusted routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,13 +6,13 @@ import Home from "./home/home";
 import SignUpContainer from "./session/signup_container";
 import LogInContainer from "./session/login_container";
 import Modal from "./modal/modal";
-import PlanScheduleComponent from "./school/plan_schedule_component";
+import selectSchoolComponent from "./school/plan_schedule_component";
 
 export default () => (
   <div className="app">
     <Modal />
     <Route path="/" component={NavBarContainer} />
-    <Route path="/planSchedule" component={PlanScheduleComponent} />
+    <Route path="/selectSchool" component={selectSchoolComponent} />
     <Route exact path="/" component={Home} />
   </div>
 );
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default () => (
   <div className="home">
     <h1>Welcome to SamuraiCourses</h1>
-    <Link className="nav-btn" to="/planSchedule">
+    <Link className="nav-btn" to="/selectSchool">
       Plan a schedule!
     </Link>
   </div>
