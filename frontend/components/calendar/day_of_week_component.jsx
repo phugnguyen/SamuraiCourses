99c24a8dccd9ba04e5e8b368426f2b2bf6fd70d1
Phu <phugnguyen@gmail.com>
2019-05-24 14:45:08 -0700

    style daily schedule

--- frontend/components/calendar/day_of_week_component.jsx
+++ frontend/components/calendar/day_of_week_component.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { start } from "repl";
 
-class DayOfWeek extends React.Component {}
+{
+  /* <DayOfWeek
+    calenderHeight={calenderHeight}
+    pixPerHour={pixPerHour}
+    courses={monday}
+    begin={begin}
+    end={end}
+/> */
+}
+
+class DayOfWeek extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  styleDiv(course) {
+    const { pixPerHour, begin } = this.props;
+    const { start_time, end_time } = courses;
+    const divHeight = (end_time - start_time) * pixPerHour;
+    const topOffset = (start_time - begin) * pixPerHour;
+
+    const style = {
+      position: "absolute",
+      height: divHeight,
+      top: topOffset
+    };
+    return <div style={style}>{course.code + " " + course.course_number}</div>;
+  }
+
+  render() {
+    const { calendarHeight, courses } = this.props;
+    let dailySchedule = courses.map(course => styleDiv(course));
+    const dailyStyle = { height: calendarHeight, position: "relative" };
+
+    return <div style={dailyStyle}>{dailySchedule}</div>;
+  }
+}
 
 export default DayOfWeek;
