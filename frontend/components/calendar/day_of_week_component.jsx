0f37586627aec650f3b86c23c3e6b9382a242f22
Phu <phugnguyen@gmail.com>
2019-05-24 15:04:51 -0700

    day of week component inline styling

--- frontend/components/calendar/day_of_week_component.jsx
+++ frontend/components/calendar/day_of_week_component.jsx
@@ -17,24 +17,36 @@ class DayOfWeek extends React.Component {
     this.state = {};
   }
 
+  // convert diff if in format 115 for 1 hour 15 mins
+  // or anything over 60 minutes
+  convertToMin(duration) {
+    return (duration / 100) * 60 + (diff % 100);
+  }
+
   styleDiv(course) {
     const { pixPerHour, begin } = this.props;
-    const { start_time, end_time } = courses;
-    const divHeight = (end_time - start_time) * pixPerHour;
-    const topOffset = (start_time - begin) * pixPerHour;
+    const { start_time, end_time, code, course_number } = course;
 
+    let duration = end_time - start_time;
+    duration < 60 ? (duration /= 60) : (duration = this.convertToMin(duration));
+
+    const divHeight = duration * pixPerHour;
+
+    let topOffset = start_time - begin;
+
+    // TODO: properly do inline style
     const style = {
       position: "absolute",
-      height: divHeight,
-      top: topOffset
+      height: divHeight + "px",
+      top: topOffset + "px"
     };
-    return <div style={style}>{course.code + " " + course.course_number}</div>;
+    return <div style={style}>{code + " " + course_number}</div>;
   }
 
   render() {
     const { calendarHeight, courses } = this.props;
-    let dailySchedule = courses.map(course => styleDiv(course));
-    const dailyStyle = { height: calendarHeight, position: "relative" };
+    let dailySchedule = courses.map(course => this.styleDiv(course));
+    const dailyStyle = { height: calendarHeight + "px", position: "relative" };
 
     return <div style={dailyStyle}>{dailySchedule}</div>;
   }
