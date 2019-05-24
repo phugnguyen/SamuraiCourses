3f32d3dfffe9a17e5170a4733b909379b5c3f8cc
Phu <phugnguyen@gmail.com>
2019-05-24 13:27:48 -0700

    seed data for calendar component

--- frontend/components/calendar/calendar_component.jsx
+++ frontend/components/calendar/calendar_component.jsx
@@ -6,11 +6,49 @@ class Calendar extends React.Component {
     this.state = {};
   }
 
-  render(){
-      
-      return ();
-  }
+  render() {
+    // Generate [blank, M, T, W, R, F]
+    // Generate time range
 
+    // props input is an array where each element is a course_Id
+    // find time range
+
+    const schedule = [
+      {
+        start_time: "900",
+        end_time: "950",
+        days: "MWF",
+        code: "KOR",
+        course_number: "10"
+      },
+      {
+        start_time: "1000",
+        end_time: "1050",
+        days: "MWF",
+        code: "MATH",
+        course_number: "10"
+      },
+      {
+        start_time: "900",
+        end_time: "950",
+        days: "TR",
+        code: "CMPSC",
+        course_number: "10"
+      },
+      {
+        start_time: "1000",
+        end_time: "1050",
+        days: "TR",
+        code: "PHIL",
+        course_number: "10"
+      }
+    ];
+    return (
+      <div>
+        <h3>Schedule here</h3>
+      </div>
+    );
+  }
 }
 
 export default Calendar;
